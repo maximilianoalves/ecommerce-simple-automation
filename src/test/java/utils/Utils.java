a7309c55a882c3e2bd140e6fa3af4431a482a271
Maximiliano <maximilianodacruz@gmail.com>
2019-08-09 18:04:19 -0300

    add changes for select os

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -3,7 +3,12 @@ package utils;
 public class Utils {
 
     public static String webdriverLocation() {
-        return "./driver/chromedriver";
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("mac")){
+            return "./driver/chromedriver";
+        } else {
+            return "./driver/chromedriver.exe";
+        }
     }
 
     public static String getBaseUrl() { return "http://automationpractice.com/"; }
